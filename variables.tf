df786b37ca77d00ed581a4669e2d2f2aed9650fa
Anton Melekhin <44847512+antmelekhin@users.noreply.github.com>
2025-04-23 19:25:41 +0300

    feat: add merge variables (#1)

--- variables.tf
+++ variables.tf
@@ -31,6 +31,60 @@ variable "visibility_level" {
   default     = "private"
 }
 
+variable "allow_merge_on_skipped_pipeline" {
+  description = "Set to `true` if you want to treat skipped pipelines as if they finished with success."
+  type        = bool
+  default     = false
+}
+
+variable "merge_method" {
+  description = "Set the merge method. Valid values are `merge`, `rebase_merge`, `ff`."
+  type        = string
+  default     = "merge"
+}
+
+variable "merge_pipelines_enabled" {
+  description = "Enable or disable merge pipelines."
+  type        = bool
+  default     = false
+}
+
+variable "merge_requests_access_level" {
+  description = "Set the merge requests access level. Valid values are `disabled`, `private`, `enabled`."
+  type        = string
+  default     = "enabled"
+}
+
+variable "merge_requests_enabled" {
+  description = "Enable merge requests for the project."
+  type        = bool
+  default     = true
+}
+
+variable "merge_trains_enabled" {
+  description = "Enable or disable merge trains. Requires `merge_pipelines_enabled` to be set to `true` to take effect."
+  type        = bool
+  default     = false
+}
+
+variable "only_allow_merge_if_all_discussions_are_resolved" {
+  description = "Set to `true` if you want allow merges only if all discussions are resolved."
+  type        = bool
+  default     = false
+}
+
+variable "only_allow_merge_if_pipeline_succeeds" {
+  description = "Set to `true` if you want allow merges only if a pipeline succeeds."
+  type        = bool
+  default     = false
+}
+
+variable "remove_source_branch_after_merge" {
+  description = "Enable `Delete source branch` option by default for all new merge requests."
+  type        = bool
+  default     = false
+}
+
 variable "archived" {
   description = "Whether the project is in read-only mode (archived). "
   type        = bool
